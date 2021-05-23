2d922446e9b6af9bfc76345476772353038f7526
BritishPandaChick <sbartley1080@gmail.com>
2021-05-23 14:04:00 -0400

    Configure plugins

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1622305423;s:7:"authKey";s:64:"Hq$w%3EXYv{U]^eSIb<pmak9Bm|-|Qih;N:X7Fq-.W(L4~t(8D{T+Yb``0%eS7:K";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1004;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"Hq$w%3EXYv{U]^eSIb<pmak9Bm|-|Qih;N:X7Fq-.W(L4~t(8D{T+Yb``0%eS7:K";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1004;}
